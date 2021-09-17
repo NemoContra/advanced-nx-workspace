7f585c14ddfaa8a3b5dd48ebebc5cb86f37ce691
Florian Tischler <florian.tischler@gmx.net>
2021-09-17 17:42:29 +0200

    solution

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -1,24 +1,30 @@
-import {HttpClientModule} from "@angular/common/http";
-import {NgModule} from "@angular/core";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {RouterModule} from "@angular/router";
-import {FlightLibModule} from "@flight-workspace/flight-lib";
-import {LoggerModule} from "@flight-workspace/logger-lib";
-import {ReactiveComponentModule} from "@ngrx/component";
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FlightLibModule } from '@flight-workspace/flight-lib';
+import { LoggerModule } from '@flight-workspace/logger-lib';
+import { ReactiveComponentModule } from '@ngrx/component';
+import { EffectsModule } from '@ngrx/effects';
+import { RouterState, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { metaReducers, reducers } from './+state';
 
-import {AppComponent} from "./app.component";
-import {APP_ROUTES} from "./app.routes";
-import {BasketComponent} from "./basket/basket.component";
-import {FlightBookingModule} from "./flight-booking/flight-booking.module";
-import {FlightCancellingModule} from "./flight-booking/flight-cancelling/flight-cancelling.module";
-import {FlightLookaheadComponent} from "./flight-lookahead/flight-lookahead.component";
-import {HomeComponent} from "./home/home.component";
-import {NavbarComponent} from "./navbar/navbar.component";
-import {CustomLogFormatterService} from "./shared/logging/custom-log-formatter.service";
-import {SharedModule} from "./shared/shared.module";
-import {SidebarComponent} from "./sidebar/sidebar.component";
+import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
+import { BasketComponent } from './basket/basket.component';
+import { FlightBookingModule } from './flight-booking/flight-booking.module';
+import { FlightCancellingModule } from './flight-booking/flight-cancelling/flight-cancelling.module';
+import { FlightLookaheadComponent } from './flight-lookahead/flight-lookahead.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomLogFormatterService } from './shared/logging/custom-log-formatter.service';
+import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
@@ -38,7 +44,14 @@ import {SidebarComponent} from "./sidebar/sidebar.component";
     }),
     FormsModule,
     ReactiveFormsModule,
-    ReactiveComponentModule
+    ReactiveComponentModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+      routerState: RouterState.Minimal,
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   declarations: [
     AppComponent,
@@ -51,5 +64,4 @@ import {SidebarComponent} from "./sidebar/sidebar.component";
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
